656705111fa9e306ccfd865e38b2a794254ae582
Kevin Clemons <10484164@CS518-03.ad.uvu.edu>
2016-04-07 09:44:16 -0600

    Added pause mechanic. Scene adjustments.

---
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			TogglePause();
+		}
+	}
+
+	public void TogglePause() {
+		Time.timeScale = (Time.timeScale == 0)? 1 : 0;
+	}
+}
